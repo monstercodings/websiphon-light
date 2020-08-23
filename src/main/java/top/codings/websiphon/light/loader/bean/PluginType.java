655d6b69635bce6f309c35ac5f9319baecc2a2b4
何好听 <monstercodings@163.com>
2020-08-24 00:30:28 +0800

    常规优化

--- src/main/java/top/codings/websiphon/light/loader/bean/PluginType.java
+++ src/main/java/top/codings/websiphon/light/loader/bean/PluginType.java
@@ -1,7 +1,16 @@
 package top.codings.websiphon.light.loader.bean;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum PluginType {
-    REQUESTER(),
-    HANDLER(),
-    PROCESSOR(),
+    REQUESTER("网络请求器"),
+    HANDLER("响应管理器"),
+    PROCESSOR("响应处理器"),
+    ;
+    private String desc;
+
+    public String desc() {
+        return desc;
+    }
 }
